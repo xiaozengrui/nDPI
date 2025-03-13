1eadafec2d9f4c244b6c4a9a11bfb03bd2e56222
zengfanyong@secsmart.net <zengfanyong@secsmart.net>
2025-03-13 15:40:35 +0800

    perf(编译):modify build error.

--- src/lib/ndpi_main.c
+++ src/lib/ndpi_main.c
@@ -2482,7 +2482,7 @@ static void ndpi_init_protocol_defaults(struct ndpi_detection_module_struct *ndp
                           ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
   ndpi_set_proto_defaults(ndpi_str, 1 /* cleartext */, 0 /* nw proto */, NDPI_PROTOCOL_ACCEPTABLE, NDPI_PROTOCOL_XML_RPC,
-                          "XML-RPC", NDPI_PROTOCOL_CATEGORY_RPC,
+                          "XML-RPC", NDPI_PROTOCOL_CATEGORY_RPC, NDPI_PROTOCOL_QOE_CATEGORY_UNSPECIFIED,
                           ndpi_build_default_ports(ports_a, 0, 0, 0, 0, 0) /* TCP */,
                           ndpi_build_default_ports(ports_b, 0, 0, 0, 0, 0) /* UDP */);
 
